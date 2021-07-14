c1c873bd4cb20430d8940ba198f29ffa394ec3de
MM-Morsi <m.morsy.ds@gmail.com>
2021-07-14 18:33:33 +0200

    play around with express hello world in test.js

--- projects/weather-journal-app/commentsOnlyJS/test.js
+++ projects/weather-journal-app/commentsOnlyJS/test.js
@@ -1,4 +1,5 @@
 // Load HTTP module
+/*
 const http = require("http");
 
 const hostname = "127.0.0.1";
@@ -10,3 +11,17 @@ const server = http.createServer( function (req, res){
     res.end();
 
 }).listen(8000);
+*/
+
+// learn express steps 
+var express = require('express')
+var app = express();
+const port = 3000
+
+app.get('/', function(req,res){
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`Puthtis in your broweser to open the application:  localhost:${port}`);
+})
