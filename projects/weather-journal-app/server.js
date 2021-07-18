98c9659f2eccb1e3823a4cf6c7c73ebbbb183d26
MM-Morsi <m.morsy.ds@gmail.com>
2021-07-18 21:15:39 +0200

    finished api and fetch() >> APIs & Routes (second option)

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -9,19 +9,22 @@ const express =  require("express");
 // Start up an instance of app
 const app = express()
 
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // json request body into JS object into req.body
 app.use(bodyParser.json());
 // app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
+// app.get('/reqBody', (req, res) => {
+//     console.log("wihout the body parser, you can see nothing. \nHowever, an empty JS object appears with the body parser")
+//     res.send(req.body);
+// })
 
 // Cors for cross origin allowance
-app.use(cors())
+app.use(cors());    // for communication with the API (security wise) - allows API to be used by a foreign device
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static('website')); // makes website three files accecible >> endpoints
 
 // Setup Server
 app.listen(port, ()=>{
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -3,3 +3,22 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// API Url to get the weather data using fetch
+const key = '306f34f6f645e03e6d803023522e7bed';
+const generateButton = document.querySelector("#generate");
+generateButton.addEventListener("click", async ()=>{ // await works inside async functions
+    let zip = document.querySelector("#zip").value;
+    console.log(zip)
+    if (!zip){
+        console.log("zip code empty")
+        return
+    }
+    // async function >> await for the api response
+    const tempReq = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}&units=metric`);
+    const tempReqJSObj = await tempReq.json();
+    console.log("response as a Json object:\n", tempReq);
+    console.log("response as a javascript object:\n", tempReqJSObj);
+    console.log("temperature is: ", tempReqJSObj.main.temp);
+    const temperature = tempReqJSObj.main.temp;
+})
