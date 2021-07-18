e7be63c40f855411f6106764f26280b5a209feab
MM-Morsi <m.morsy.ds@gmail.com>
2021-07-18 18:55:25 +0200

    update server.js the actual file not commentsonly edition

--- projects/weather-journal-app/commentsOnlyJS/server.js
+++ projects/weather-journal-app/commentsOnlyJS/server.js
@@ -1,19 +1,26 @@
 // Setup empty JS object to act as endpoint for all routes
-projectDate = {};
+
+
 // Express to run server and routes
-var express =  require("express");
-var app = express();
+
+
 // Start up an instance of app
 
-/* Dependencies */
+
 /* Middleware*/
-app.use
 //Here we are configuring express to use body-parser as middle-ware.
+
+
+
 // Cors for cross origin allowance
+
 
 // Initialize the main project folder
 
+
 // Spin up the server
+
+
 // Callback to debug
 
 // Initialize all route with a callback function
